2929112e71582ee8fa73777c42400cae9ac4fcd2
korlahwarleh <jmkolawole@gmail.com>
2022-01-26 09:21:29 +0100

    fix js

--- resources/views/frontend/partials/_javascript.blade.php
+++ resources/views/frontend/partials/_javascript.blade.php
@@ -1,31 +1,31 @@
-<script src="{{asset('js/frontend/js/vendor/jquery-3.3.1.min.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/vendor/jquery-3.3.1.min.js')}}"></script>
 
-<script src="{{asset('js/frontend/js/jquery.countdown.min.js')}}"></script>
-
-
-<script src="{{asset('js/frontend/js/jquery.meanmenu.min.js')}}"></script>
-
-<script src="{{asset('js/frontend/js/jquery.scrollUp.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/jquery.countdown.min.js')}}"></script>
 
 
-<script src="{{asset('js/frontend/js/jquery.fancybox.min.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/jquery.meanmenu.min.js')}}"></script>
+
+<script src="{{secure_asset('js/frontend/js/jquery.scrollUp.js')}}"></script>
 
 
-<script src="{{asset('js/frontend/js/jquery-ui.min.js')}}"></script>
-
-<script src="{{asset('js/frontend/js/owl.carousel.min.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/jquery.fancybox.min.js')}}"></script>
 
 
-<script src="{{asset('js/frontend/js/popper.min.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/jquery-ui.min.js')}}"></script>
+
+<script src="{{secure_asset('js/frontend/js/owl.carousel.min.js')}}"></script>
 
 
-<script src="{{asset('js/frontend/js/bootstrap.min.js')}}"></script>
-
-<script src="{{asset('js/frontend/js/bootstrap-notify.min.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/popper.min.js')}}"></script>
 
 
-<script src="{{asset('js/frontend/js/plugins.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/bootstrap.min.js')}}"></script>
+
+<script src="{{secure_asset('js/frontend/js/bootstrap-notify.min.js')}}"></script>
+
+
+<script src="{{secure_asset('js/frontend/js/plugins.js')}}"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-<script src="{{asset('js/frontend/js/custom.js')}}"></script>
+<script src="{{secure_asset('js/frontend/js/custom1.js')}}"></script>
